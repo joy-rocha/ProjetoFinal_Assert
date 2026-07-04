7ae5b4cb24fb51f141ee219e7ee447c10400c2c0
Joyce Evellyn <joyce.santana@academico.ifpb.edu.br>
2026-07-04 09:27:30 -0300

    feat primeiro commit

---
+++ Core/Inc/Button.h
@@ -0,0 +1,45 @@
+/**
+ * @file    [NOME_DO_MODULO].h
+ * @brief   [Escreva aqui a responsabilidade do módulo, ex: Abstração de hardware]
+ * @author  [Seu Nome]
+ * @date    [Data de Hoje]
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+#endif /* INC_BUTTON_H_ */
+
+/* ============================================================================== */
+/* Includes                                                                       */
+/* ============================================================================== */
+#include <stdint.h>
+#include <stdbool.h>
+/* Inclua aqui outros .h necessários, como main.h se precisar de tipos básicos */
+
+/* ============================================================================== */
+/* Defines Locais                                                                 */
+/* ============================================================================== */
+
+
+/* ============================================================================== */
+/* Constantes                                                                     */
+/* ============================================================================== */
+
+
+/* ============================================================================== */
+/* Estruturas de Dados Locais                                                     */
+/* ============================================================================== */
+
+
+/* ============================================================================== */
+/* Protótipos Locais / Funções Públicas                                           */
+/* ============================================================================== */
+
+/**
+ * @brief  Exemplo de inicialização do módulo.
+ * @param  Nenhum.
+ * @retval Nenhum.
+ */
+void [NomeDoModulo]_Init(void);
+
+#endif /* INC_[NOME_DO_MODULO_EM_MAIUSCULO]_H_ */
---
+++ Core/Inc/LedPwm.h
@@ -0,0 +1,45 @@
+/**
+ * @file    [NOME_DO_MODULO].h
+ * @brief   [Escreva aqui a responsabilidade do módulo, ex: Abstração de hardware]
+ * @author  [Seu Nome]
+ * @date    [Data de Hoje]
+ */
+
+#ifndef INC_LEDPWM_H_
+#define INC_LEDPWM_H_
+#endif /* INC_LEDPWM_H_ */
+
+/* ============================================================================== */
+/* Includes                                                                       */
+/* ============================================================================== */
+#include <stdint.h>
+#include <stdbool.h>
+/* Inclua aqui outros .h necessários, como main.h se precisar de tipos básicos */
+
+/* ============================================================================== */
+/* Defines Locais                                                                 */
+/* ============================================================================== */
+
+
+/* ============================================================================== */
+/* Constantes                                                                     */
+/* ============================================================================== */
+
+
+/* ============================================================================== */
+/* Estruturas de Dados Locais                                                     */
+/* ============================================================================== */
+
+
+/* ============================================================================== */
+/* Protótipos Locais / Funções Públicas                                           */
+/* ============================================================================== */
+
+/**
+ * @brief  Exemplo de inicialização do módulo.
+ * @param  Nenhum.
+ * @retval Nenhum.
+ */
+void [NomeDoModulo]_Init(void);
+
+#endif /* INC_[NOME_DO_MODULO_EM_MAIUSCULO]_H_ */
